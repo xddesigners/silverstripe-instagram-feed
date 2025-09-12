748e223abf22de80119982e891c6e466eacb3b0c
Remy Vaartjes <remy@xd.nl>
2025-09-12 11:24:38 +0200

    fix buildtask v6

--- src/Tasks/InstagramUpdateTask.php
+++ src/Tasks/InstagramUpdateTask.php
@@ -3,17 +3,25 @@
 namespace XD\InstagramFeed\Tasks;
 
 use SilverStripe\Dev\BuildTask;
+use SilverStripe\PolyExecution\PolyOutput;
+use Symfony\Component\Console\Input\InputInterface;
 use XD\InstagramFeed\Clients\InstagramClient;
 
 class InstagramUpdateTask extends BuildTask
 {
-    protected $title = 'Instagram Update Task';
+    protected string $title = 'Instagram Update Task';
 
-    protected $description = 'Update Instagram feed data';
+    protected static string  $description = 'Update Instagram feed data';
 
     protected $enabled = true;
 
-    public function run($request)
+    // implement execute method
+    public function execute(InputInterface $input, PolyOutput $output): int
+    {
+        return $this->run($input, $output);
+    }
+
+    public function run(InputInterface $input, PolyOutput $output): int
     {
         $client = new InstagramClient();
         $client->updateCachedUserMedia();
